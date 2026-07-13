96cd738cdbcc4633bbef573f7cdc07613b5ac070
Bobby <boblaveyou@gmail.com>
2026-07-13 21:14:05 +0700

    refactor: remove path aliases from tsconfig.json in favor of SvelteKit configuration

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,10 @@ const config = {
 
     kit: {
         // ใช้ adapter-auto ตามโค้ดต้นฉบับของโปรเจกต์นี้
-        adapter: adapter()
+        adapter: adapter(),
+        alias: {
+            $static: './static'
+        }
     }
 };
 
